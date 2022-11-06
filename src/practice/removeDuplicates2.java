0c6aec1517da3a991406d04dac00753166838027
Ain Nurizzati <ainnurizzati24@gmail.com>
2022-11-06 20:15:52 +0800

    Initial commit

---
+++ src/practice/removeDuplicates2.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class removeDuplicates2 {
+
+	public static void main(String[] args) {
+		int [] age = {20,21,25,19};
+		
+		int length = age.length;
+		
+		/* for(int i=0; i<length; i++) {
+			if(age[i] = age[i+1]) {
+				return age[i]
+			}
+		} */
+		
+		
+
+	}
+
+}
